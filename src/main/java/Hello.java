b075ad98307cb20c4f46ee4ae3a0d51bbd24438c
yakup <manajer166@gmail.com>
2021-06-21 16:26:47 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("developing hello feature");
+        System.out.println("Local Repo is already published");
+        System.out.println("doing more works");
     }
 }
